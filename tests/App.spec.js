a1fb70fefd3ef51bf284008b461768194b1bfdba
Tinita <tino4ka.87@mail.ru>
2024-05-17 12:02:10 +0300

    Project modified

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,16 +1,9 @@
 const { test, expect } = require("@playwright/test");
-const { chromium } = require("playwright");
 const { password } = require('./user.js');
 const { email } = require('./user.js');
 
-test("successfulAuthorization", async () => {
-  test.slow();
-  const browser = await chromium.launch({
-    headless: false,
-    slowMo: 5000,
-    devtools: true
-  });
-  const page = await browser.newPage();
+test("successfulAuthorization", async ({ page }) => {
+
   await page.goto("https://netology.ru/?modal=sign_in");
   await page.getByPlaceholder('Email').fill(email);
   await page.fill('[placeholder="Пароль"]', password);
@@ -19,17 +12,10 @@ test("successfulAuthorization", async () => {
   await expect(page).toHaveURL("https://netology.ru/profile/8738514");
   await expect(page).toHaveTitle('Моё обучение');
   await page.screenshot({ path: 'successScreenshot.png'});
-  await browser.close();
 });
 
-test("unsuccessfulAuthorization", async () => {
-  test.slow();
-  const browser = await chromium.launch({
-    headless: false,
-    slowMo: 5000,
-    devtools: true
-  });
-  const page = await browser.newPage();
+test("unsuccessfulAuthorization", async ({ page }) => {
+  
   await page.goto("https://netology.ru/?modal=sign_in");
   await page.getByPlaceholder('Email').fill('tino4ka@mail.ru');
   await page.fill('[placeholder="Пароль"]', '123');
@@ -37,5 +23,4 @@ test("unsuccessfulAuthorization", async () => {
 
   await expect(page.getByTestId('login-error-hint')).toHaveText('Вы ввели неправильно логин или пароль');
   await page.screenshot({ path: 'unsuccessScreenshot.png' });
-  await browser.close();
 });
